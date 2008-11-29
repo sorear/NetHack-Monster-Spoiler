9a441d4dd7e85711383723c04898f5c505ab34fa
Stefan O'Rear <stefanor@cox.net>
2008-11-29 05:59:52 -0500

    Initial import of NetHack::Monster

---
+++ lib/NetHack/Monster.pm
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+package NetHack::Monster;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+NetHack::Monster - ???
+
+=head1 SYNOPSIS
+
+    use NetHack::Monster;
+
+=head1 DESCRIPTION
+
+
+
+=head1 AUTHOR
+
+Stefan O'Rear, C<< <stefanor@cox.net> >>
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-nethack-monster at rt.cpan.org>, or browse
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=NetHack-Monster>.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2008 Stefan O'Rear.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
